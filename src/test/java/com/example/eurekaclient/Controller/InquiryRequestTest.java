09817afef376ae0797bcc44debf7c00d159adc8e
devtruemoney <devtruemoney@gmail.com>
2023-10-24 15:38:48 +0700

    JUnit

---
+++ src/test/java/com/example/eurekaclient/Controller/InquiryRequestTest.java
@@ -0,0 +1,33 @@
+package com.example.eurekaclient.Controller;
+
+import com.example.eurekaclient.Pojo.Request.InquirySNAPRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InquiryRequestTest {
+
+    static InquirySNAPRequest setRequestInquiryPositive(){
+        InquirySNAPRequest request = new InquirySNAPRequest();
+        Map<String,Object> additionalInfo = new HashMap<>();
+        additionalInfo.put("bankCd", "HNBN");
+        request.setPartnerServiceId("97720208");
+        request.setCustomerNo("781315391070");
+        request.setVirtualAccountNo("9772020870717292");
+        request.setInquiryRequestId("41807553358950093184");
+        request.setAdditionalInfo(additionalInfo);
+        return request;
+    }
+
+    static InquirySNAPRequest setRequestInquiryMandatoryAdditionalInfo(){
+        InquirySNAPRequest request = new InquirySNAPRequest();
+        Map<String,Object> additionalInfo = new HashMap<>();
+        request.setPartnerServiceId("97720208");
+        request.setCustomerNo("781315391070");
+        request.setVirtualAccountNo("9772020870717292");
+        request.setInquiryRequestId("41807553358950093184");
+        request.setAdditionalInfo(additionalInfo);
+        return request;
+    }
+
+}
